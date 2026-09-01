4da863372304c3eae1cd686f237e33adc8424453
hanuri2000-svg <hanuri2000@naver.com>
2026-09-02 01:09:20 +0900

    Bump app version to v1.0.1

--- version-badge.js
+++ version-badge.js
@@ -3,7 +3,7 @@
   if(window.__dorangVersionBadgeLoaded)return;
   window.__dorangVersionBadgeLoaded=true;
 
-  const VERSION='1.0.0';
+  const VERSION='1.0.1';
   window.DORANG_APP_VERSION=VERSION;
 
   const style=document.createElement('style');
